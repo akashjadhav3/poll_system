5161e309976e5e57e28c79424f8c9b4db6c22704
root <root@localhost.localdomain>
2020-04-15 15:43:36 +0530

    adding answer model

--- poll/models.py
+++ poll/models.py
@@ -13,12 +13,30 @@ class Question(models.Model):
     def __str__(self):
         return self.title
 
+    @property
+    def choices(self):
+        return self.choice_set.all()
 
 class Choice(models.Model):
-    question = models.ForeignKey('poll.Question', on_delete="CASCADE")
+    question = models.ForeignKey('poll.Question', on_delete="models.CASCADE")
     text = models.TextField(null=True , blank=True)
     created_at = models.ForeignKey(User,null=True, blank=True, on_delete=models.CASCADE)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.text
+
+    @property
+    def votes(self):
+        return self.answer_set.count()
+
+class Answer(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.first_name + '-' + self.choice.text
+
+
--- poll/views.py
+++ poll/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from . models import *
+from django.http import Http404
 
 def index(request):
     question = Question.objects.all()
-    context = {"question":question,"title":'Poll'}
+    context = {"question":question,"title":"Poll"}
     return render(request,'polls/index.html',context)
+
+def details(request,id=None):
+    try:
+        question = Question.objects.get(id=id)
+    except:
+        raise Http404
+    context = {"question":question}
+    return render(request,'polls/details.html',context)
